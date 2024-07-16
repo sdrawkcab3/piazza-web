90a98409f4452442c571c8e3b8940d5ed1b4fa75
Rick Marvin <sdrawkcab3@gmail.com>
2024-07-16 14:56:42 -0400

    Add logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,13 @@ class SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        log_out
+
+        flash[:success] = t(".success")
+        redirect_to root_path, status: :see_other
+    end
+
     private
         def login_params
             # The `||=` (conditional assignment) operator only assigns a value if variable is nil or false, otherwise it short-circuits to the current value
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root "feed#show"
 
-  get "sign_up",  to: "users#new"
-  post "sign_up", to: "users#create"
+  get "sign_up",    to: "users#new"
+  post "sign_up",   to: "users#create"
 
-  get "login",    to: "sessions#new"
-  post "login",   to: "sessions#create"
+  get "login",      to: "sessions#new"
+  post "login",     to: "sessions#create"
+  delete "logout",  to: "sessions#destroy"
 end
